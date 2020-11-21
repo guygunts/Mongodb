8a4fb5141c624e135d07e58b897b5647af94f094
natchanon inchanta <guygun_ts12@hotmail.com>
2020-11-21 11:04:37 +0700

    guy
    update receipt

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const usermanagement = require('../src/controller/usermanagementController');
 const food = require('../src/controller/foodmanagementController');
 const room = require('../src/controller/roommanagementController');
 const login = require('../src/controller/loginContrller');
-
+const receipt = require('../src/controller/receiptController')
 router.post('/login',login.loginuser)
 
 
@@ -25,6 +25,11 @@ router.post('/listroom',room.listroom)
 router.post('/addroom',room.addroom)
 router.post('/updateroom',room.updateroom)
 router.post('/deleteroom',room.deleteroom)
+
+router.post('/listreceipt',receipt.listreceipt)
+router.post('/addreceipt',receipt.addreceipt)
+router.post('/updatereceipt',receipt.updatereceipt)
+router.post('/deletereceipt',receipt.deletereceipt)
 
 router.get('/createdb',createdb)
 
